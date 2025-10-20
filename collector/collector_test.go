5de9be19564fd2f2aa2eb1667b37fa3c518942d9
Lukas Supienis <lukassup@users.noreply.github.com>
2025-10-20 14:45:34 +0300

    fix: tests deprecations/lints

--- collector/collector_test.go
+++ collector/collector_test.go
@@ -1,19 +1,29 @@
 package collector
 
 import (
-	"io/ioutil"
 	"os"
+	"path"
 	"testing"
 )
 
 func TestDirectoryWalk(t *testing.T) {
-	dir, _ := ioutil.TempDir("", "maildrop")
-	defer os.RemoveAll(dir)
-	file, _ := ioutil.TempFile(dir, "test_mail_file")
-	_, err := file.Write([]byte("000"))
+	dir, err := os.MkdirTemp("", "maildrop")
 	if err != nil {
 		t.Errorf("%v", err)
 	}
+	defer os.RemoveAll(dir)
+
+	file, err := os.Create(path.Join(dir, "test_mail_file"))
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+	defer file.Close()
+
+	_, err = file.Write([]byte("000"))
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
 	maildropCount, maildropSize := DirectoryWalk("", dir)
 	if maildropCount != 1 {
 		t.Errorf("Wrong file count. Wanted 1. Got %v", maildropCount)
